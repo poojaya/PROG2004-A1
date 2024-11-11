3ab4bc078a35b5139d2b2b39ff4cc9cec40e8ead
poojaya <83125968+poojaya@users.noreply.github.com>
2024-11-11 14:32:37 +1100

    Created Surgeon Class (Subclass of HealthProfessional)
    This class represents a surgeon, another specific type of health professional.

---
+++ src/Surgeon.java
@@ -0,0 +1,23 @@
+public class Surgeon extends HealthProfessional {
+    private String specialty;
+
+    // Default constructor
+    public Surgeon() {
+        super();
+        this.specialty = "";
+    }
+
+    // Parameterized constructor
+    public Surgeon(int id, String name, String department, String specialty) {
+        super(id, name, department);
+        this.specialty = specialty;
+    }
+
+    // Override printInfo() method
+    @Override
+    public void printInfo() {
+        System.out.println("Surgeon Info:");
+        super.printInfo();
+        System.out.println("Specialty: " + this.specialty);
+    }
+}
