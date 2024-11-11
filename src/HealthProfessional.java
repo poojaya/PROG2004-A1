4a50b1ba60e6bbb0141b426632e948a2f46a6d40
poojaya <83125968+poojaya@users.noreply.github.com>
2024-11-11 14:24:03 +1100

    Create Health Professional.java

---
+++ src/HealthProfessional.java
@@ -0,0 +1,27 @@
+public class HealthProfessional {
+    // Instance variables
+    private int id;
+    private String name;
+    private String department;
+
+    // Default constructor
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "";
+        this.department = "";
+    }
+
+    // Parameterized constructor
+    public HealthProfessional(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    // Method to print all instance variables
+    public void printInfo() {
+        System.out.println("ID: " + this.id);
+        System.out.println("Name: " + this.name);
+        System.out.println("Department: " + this.department);
+    }
+}
